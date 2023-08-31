2da3f83a1b85ee37a696cceeb3a86a061e0315d2
Clément Côme <clement.come98@gmail.com>
2023-08-31 22:11:42 +0200

    chore(fetch_data) Improve docker and datetime Smaller docker images datetime in Paris timezone

--- velib_spot_predictor/data/fetch_data.py
+++ velib_spot_predictor/data/fetch_data.py
@@ -1,13 +1,11 @@
 import click
 import json
 import requests
-from tqdm import tqdm
+import pytz
 from datetime import datetime
 from pathlib import Path
 
-# Replace 'YOUR_API_URL' with the actual API endpoint
 api_url = 'https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json'
-# data = fetch_data(api_url)
 
 def fetch_data(url):
     response = requests.get(url)
@@ -18,51 +16,14 @@ def fetch_data(url):
         raise Exception(f"Response status code was: {response.status_code}")
 
 
-def fetch_data_with_pagination(base_url, limit=10):
-    all_data = []
-
-    # Getting the total_count of records with a first request
-    response = requests.get(base_url, params={"limit": 0})
-    total_count = response.json().get("total_count")
-    if total_count is None:
-        raise ValueError("Could not fetch the total count of records.")
-
-    n_iter = total_count // limit + 1
-
-    for i in tqdm(range(n_iter)):
-        offset = i*limit
-        # Construct the query parameters
-        params = {
-            'limit': limit,
-            'offset': offset
-        }
-
-        # Make the API request
-        response = requests.get(base_url, params=params)
-
-        if response.status_code == 200:
-            data = response.json()
-            if not data:
-                print("No data received")
-                break  # No more data to fetch
-            try:
-                results = data["results"]
-                all_data.extend(results)  # Append current chunk to all_data
-            except Exception as e:
-                raise e
-        else:
-            print(f"Request failed with status code: {response.status_code}")
-            break
-
-    return all_data
-
-
 @click.command()
 @click.argument("save-folder", type=click.Path(exists=True, file_okay=False))
 def fetch_and_save_raw_data(save_folder: str) -> None:
-    formatted_datetime = datetime.now().strftime("%Y%m%d-%H%M%S")
+    tz = pytz.timezone("Europe/Paris")
+    datetime_now = datetime.now().astimezone(tz=tz)
+    formatted_datetime = datetime_now.strftime("%Y%m%d-%H%M%S")
 
-    click.echo(f"Fetching data at {datetime.now()}")
+    click.echo(f"Fetching data at {datetime_now}")
     data = fetch_data(api_url)
 
     filename = f"velib_availability_real_time_{formatted_datetime}.json"
