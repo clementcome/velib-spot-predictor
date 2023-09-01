238545389469938e8e4e9f9de57d1ea8267410fb
Clément Côme <clement.come98@gmail.com>
2023-09-01 18:23:04 +0200

    doc(all) Documenting everything

--- velib_spot_predictor/data/fetch_data.py
+++ velib_spot_predictor/data/fetch_data.py
@@ -1,3 +1,4 @@
+"""Fetch data from the Velib API and save it to a file."""
 import json
 from datetime import datetime
 from pathlib import Path
@@ -6,27 +7,54 @@ import click
 import pytz
 import requests
 
-api_url = "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json"
+from velib_spot_predictor.data.constants import API_URL
 
 
-def fetch_data(url):
+def fetch_data(url: str) -> list:
+    """Fetch data from the Velib API.
+
+    Parameters
+    ----------
+    url : str
+        URL of the Velib API
+
+    Returns
+    -------
+    list
+        List of information collected from the Velib API related to the
+        availability of spots in Velib stations
+
+    Raises
+    ------
+    HTTPError
+        If the response status code is not 200
+    """
     response = requests.get(url, timeout=30)
     if response.status_code == requests.codes.OK:
         data = response.json()["data"]["stations"]
         return data
     else:
-        raise Exception(f"Response status code was: {response.status_code}")
+        raise requests.exceptions.HTTPError(
+            f"Request failed with status code: {response.status_code}"
+        )
 
 
 @click.command()
 @click.argument("save-folder", type=click.Path(exists=True, file_okay=False))
 def fetch_and_save_raw_data(save_folder: str) -> None:
+    """Fetch data from the Velib API and save it to a file.
+
+    Parameters
+    ----------
+    save_folder : str
+        Path to the folder where the fetched data will be saved
+    """
     tz = pytz.timezone("Europe/Paris")
     datetime_now = datetime.now().astimezone(tz=tz)
     formatted_datetime = datetime_now.strftime("%Y%m%d-%H%M%S")
 
     click.echo(f"Fetching data at {datetime_now}")
-    data = fetch_data(api_url)
+    data = fetch_data(API_URL)
 
     filename = f"velib_availability_real_time_{formatted_datetime}.json"
     filepath = Path(save_folder) / filename
--- velib_spot_predictor/model/train_model.py
+++ velib_spot_predictor/model/train_model.py
@@ -1,9 +1,23 @@
+"""Methods used for training model."""
 import pandas as pd
 from sklearn.base import RegressorMixin
 from sklearn.linear_model import LinearRegression
 
 
 def train(data: pd.DataFrame) -> RegressorMixin:
+    """
+    Train a model to predict the number of available spots in a Velib station.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Data used for training
+
+    Returns
+    -------
+    RegressorMixin
+        Trained model
+    """
     features = data[["Identifiant station", "Heure"]]
     y = data["Nombre bornettes libres"]
     model = LinearRegression()
