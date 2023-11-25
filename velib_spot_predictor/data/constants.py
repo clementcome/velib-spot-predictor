734ed2b9a0ee3234d5a4b1d8fc4722db1bd381f9
leroux95t <97245084+leroux95t@users.noreply.github.com>
2023-11-25 17:23:23 +0100

    Update API URL.py

--- velib_spot_predictor/data/constants.py
+++ velib_spot_predictor/data/constants.py
@@ -1,3 +1,3 @@
 """Constants used in the data module."""
 
-API_URL = "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json"
+API_URL = "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json"
